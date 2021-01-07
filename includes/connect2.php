19282f8e84fefde16e8274ab9f248772dfb7d3b7
Rishabh Gupta <58866004+rishabhgupta11@users.noreply.github.com>
2021-01-07 20:20:34 +0530

    Update connect2.php

--- includes/connect2.php
+++ includes/connect2.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("localhost","root","4b86XcMhd8PHQi3d","bloc36");
+    $conn = new mysqli("localhost","root","password","bloc36");
     if($conn->connect_error){
         die("Connection Failed!".$conn->connect_error);
     }
